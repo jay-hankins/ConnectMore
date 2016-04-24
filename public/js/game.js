1e4ff96a525a81b74cce2ccaf4e1b199383d21a5
bmatt <mess@purdue.edu>
2016-04-23 21:02:13 -0400

    changed some names for clarification again

--- public/js/game.js
+++ public/js/game.js
@@ -2,8 +2,8 @@ var canvas = document.getElementById('gameCanvas');
 var width = 600;
 var height = 600;
 var lineColor = '#835f17';
-var p1Color = '#831717';
-var p2Color = '#0e4f4f';
+var p1 = '#831717';
+var p2 = '#0e4f4f';
 
 function start() {
   redraw();
