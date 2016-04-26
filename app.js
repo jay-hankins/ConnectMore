b2a7f882a8de161248a08cc4f31b724378d03823
bmatt <mess@purdue.edu>
2016-04-26 07:53:20 -0400

    trying to render leaderboard stuff

--- app.js
+++ app.js
@@ -19,12 +19,29 @@ var app = express();
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('aws-us-east-1-portal.11.dblayer.com:27607/connect-more');
+app.set('view engine', 'jade');
 
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.get('/leaderboard', function(req, res) {
+    var db = req.db;
+    var collection = db.get('scores');
+    collection.find({},{},function(e,scores) {
+        res.render('index');
+    })
+});
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+app.post('/postScore', function(req, res) {
+
+});
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
